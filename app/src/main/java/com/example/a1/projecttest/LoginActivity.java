801bc265552dad780d8a86a39a011386defe8a03
Kisiev Valeri <kisivaleri@gmail.com>
2017-02-20 15:39:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a1/projecttest/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.a1.projecttest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        Button registrationBT = (Button) findViewById(R.id.registrationBT);
+        registrationBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/a1/projecttest/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.a1.projecttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 1 on 20.02.2017.
+ */
+
+public class RegistrationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_activity);
+    }
+}
