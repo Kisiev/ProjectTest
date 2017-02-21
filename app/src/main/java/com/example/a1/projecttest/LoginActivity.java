0beef23ea7ab5fddd7104be4abe0cfde04dac0d1
Kisiev Valeri <kisivaleri@gmail.com>
2017-02-21 14:44:03 +0300

    addAdaptForScreen

--- app/src/main/java/com/example/a1/projecttest/LoginActivity.java
+++ app/src/main/java/com/example/a1/projecttest/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.example.a1.projecttest;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothClass;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends Activity {
 
@@ -13,6 +17,8 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         onButtonClick();
+        ImageView imageView = (ImageView) findViewById(R.id.log_imageView);
+        createImage(R.id.log_imageView, R.mipmap.logo, imageView);
     }
     private void onButtonClick (){
         Button registrationBT = (Button) findViewById(R.id.registrationBT);
@@ -32,4 +38,9 @@ public class LoginActivity extends Activity {
             }
         });
     }
+    public void createImage(int imageId, int imageResource, ImageView imageView){
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(imageResource);
+    }
+
 }
--- app/src/main/java/com/example/a1/projecttest/RegistrationActivity.java
+++ app/src/main/java/com/example/a1/projecttest/RegistrationActivity.java
@@ -2,15 +2,16 @@ package com.example.a1.projecttest;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-/**
- * Created by 1 on 20.02.2017.
- */
+import android.widget.ImageView;
 
 public class RegistrationActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_activity);
+        ImageView imageView = (ImageView) findViewById(R.id.reg_imageView);
+        LoginActivity loginActivity = new LoginActivity();
+        loginActivity.createImage(R.id.reg_imageView, R.mipmap.logo, imageView);
     }
+
 }
