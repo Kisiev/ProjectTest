37b0f79949f57757630b49e14ad68fc67d1aca76
Kisiev Valeri <kisivaleri@gmail.com>
2017-03-10 09:48:04 +0300

    addDB

---
+++ app/src/main/java/com/example/a1/projecttest/Entities/UsersEntity.java
@@ -0,0 +1,43 @@
+package com.example.a1.projecttest.Entities;
+
+
+import com.example.a1.projecttest.AppDatabase;
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.ConflictAction;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+@Table(database = AppDatabase.class, insertConflict = ConflictAction.REPLACE)
+public class UsersEntity extends BaseModel{
+    @PrimaryKey()
+    private int id;
+    @Column(name = "login")
+    private String login;
+    @Column (name = "password")
+    private String pass;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
