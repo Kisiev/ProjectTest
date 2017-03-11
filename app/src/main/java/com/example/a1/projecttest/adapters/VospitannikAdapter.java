3c94267eaca4dc1b72e960a743e5c9dcb24f4623
Kisiev <kisivaleri@gmail.com>
2017-03-11 13:06:28 +0300

    commit

--- app/src/main/java/com/example/a1/projecttest/adapters/VospitannikAdapter.java
+++ app/src/main/java/com/example/a1/projecttest/adapters/VospitannikAdapter.java
@@ -1,6 +1,9 @@
 package com.example.a1.projecttest.adapters;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +13,19 @@ import android.widget.TextView;
 
 import com.example.a1.projecttest.R;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.Inflater;
+import java.util.Random;
 
 public class VospitannikAdapter extends RecyclerView.Adapter<VospitannikAdapter.VospitannikHolder>{
 
     List<String> serviceName;
-
-    public VospitannikAdapter (List<String> serviceName) {
+    List<Integer> colors;
+    List<String> time;
+    public VospitannikAdapter (List<String> serviceName, List<Integer> colors, List<String> time) {
+        this.time = time;
         this.serviceName = serviceName;
+        this.colors = colors;
     }
 
     @Override
@@ -32,14 +39,12 @@ public class VospitannikAdapter extends RecyclerView.Adapter<VospitannikAdapter.
     public void onBindViewHolder(final VospitannikHolder holder, int position) {
         String pos = serviceName.get(position);
         holder.textView.setText(pos);
-        holder.progressBar.setMax(5);
-        holder.progressBar.setProgress(3);
-        holder.progressPitanie.setMax(5);
-        holder.progressPitanie.setProgress(3);
-        holder.textView.setOnClickListener(new View.OnClickListener() {
+        holder.timeTv.setText(time.get(position));
+        holder.cardView.setCardBackgroundColor(colors.get(position));
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                holder.progressPitanie.setVisibility(View.VISIBLE);
+               holder.false_tv.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -51,14 +56,17 @@ public class VospitannikAdapter extends RecyclerView.Adapter<VospitannikAdapter.
 
     class VospitannikHolder extends RecyclerView.ViewHolder{
         TextView textView;
-        ProgressBar progressBar;
-        ProgressBar progressPitanie;
+        TextView timeTv;
+        CardView cardView;
+        TextView false_tv;
 
         public VospitannikHolder(View itemView) {
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.name_service);
-            progressBar = (ProgressBar) itemView.findViewById(R.id.progressBar);
-            progressPitanie = (ProgressBar) itemView.findViewById(R.id.progres_pitanie);
+            cardView = (CardView) itemView.findViewById(R.id.card_lay_vospit);
+            timeTv = (TextView) itemView.findViewById(R.id.timeTVinCard);
+            false_tv = (TextView) itemView.findViewById(R.id.false_tv);
+
         }
     }
 }
--- app/src/main/java/com/example/a1/projecttest/fragments/VospitannikFragment.java
+++ app/src/main/java/com/example/a1/projecttest/fragments/VospitannikFragment.java
@@ -29,13 +29,52 @@ public class VospitannikFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.vospitanik_fragment, container, false);
+        List<String> time = new ArrayList<>();
+        time.add("7:00 - 8:00");
+        time.add("8:00 - 8:15");
+        time.add("8:15 - 8:30");
+        time.add("8:30 - 9:10");
+        time.add("9:10 - 12:00");
+        time.add("9:10 - 12:00");
+        time.add("12:00 - 12:10");
+        time.add("12:10 - 13:10");
+        time.add("13:10 - 15:10");
+        time.add("15:10 - 15:30");
+        time.add("15:10 - 16:00");
         List<String> listService = new ArrayList<>();
-        listService.add("Отдых");
-        listService.add("Сон");
-        listService.add("Хавчик");
+        listService.add("Утренний прием детей на улице");
+        listService.add("Утреняя гимнастика");
+        listService.add("Свободная деятельность");
+        listService.add("Завтрак");
+        listService.add("Подготовка к завтраку, завтрак");
+        listService.add("Подготовка к прогулке, прогулка");
+        listService.add("Гигиенические процедуры");
+        listService.add("Подготовка к обеду, обед");
+        listService.add("Подготовка ко сну, дневной сон");
+        listService.add("Подъем, закаливающие процедуры");
+        listService.add("Подготовка к полднику, полдник");
+        List<Integer> colors = new ArrayList<>();
+        colors.add(getResources().getColor(R.color.color1));
+        colors.add(getResources().getColor(R.color.color2));
+        colors.add(getResources().getColor(R.color.color3));
+        colors.add(getResources().getColor(R.color.color4));
+        colors.add(getResources().getColor(R.color.color5));
+        colors.add(getResources().getColor(R.color.color6));
+        colors.add(getResources().getColor(R.color.color1));
+        colors.add(getResources().getColor(R.color.color2));
+        colors.add(getResources().getColor(R.color.color3));
+        colors.add(getResources().getColor(R.color.color4));
+        colors.add(getResources().getColor(R.color.color5));
+        colors.add(getResources().getColor(R.color.color6));
+        colors.add(getResources().getColor(R.color.color1));
+        colors.add(getResources().getColor(R.color.color2));
+        colors.add(getResources().getColor(R.color.color3));
+        colors.add(getResources().getColor(R.color.color4));
+        colors.add(getResources().getColor(R.color.color5));
+        colors.add(getResources().getColor(R.color.color6));
         recyclerView = (RecyclerView) view.findViewById(R.id.vospit_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(new VospitannikAdapter(listService));
+        recyclerView.setAdapter(new VospitannikAdapter(listService, colors, time));
 
         return view;
     }
