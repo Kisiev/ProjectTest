ed1d61f6942a4024241dbb2174096a200b5dbda5
Kisiev <kisivaleri@gmail.com>
2017-03-11 11:08:11 +0300

    commit

--- app/src/main/java/com/example/a1/projecttest/RegistrationActivity.java
+++ app/src/main/java/com/example/a1/projecttest/RegistrationActivity.java
@@ -1,8 +1,11 @@
 package com.example.a1.projecttest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 
 public class RegistrationActivity extends Activity {
     @Override
@@ -12,6 +15,16 @@ public class RegistrationActivity extends Activity {
         ImageView imageView = (ImageView) findViewById(R.id.reg_imageView);
         LoginActivity loginActivity = new LoginActivity();
         loginActivity.createImage(R.id.reg_imageView, R.mipmap.logo, imageView);
+
+        final RadioButton radioButton = (RadioButton) findViewById(R.id.child_RB);
+        radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(radioButton.isChecked()){
+                    startActivity(new Intent(RegistrationActivity.this, ChildActivity_.class));
+                }
+            }
+        });
     }
 
 }
