64f83c462ddf3889b4a3da70050e922f699fc62c
Kisiev Valeri <kisivaleri@gmail.com>
2017-03-31 19:26:12 +0400

    ViewPager (need fixed)

--- app/src/main/java/com/example/a1/projecttest/zavedushaia/MainZavDetSad.java
+++ app/src/main/java/com/example/a1/projecttest/zavedushaia/MainZavDetSad.java
@@ -6,21 +6,44 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 import com.example.a1.projecttest.Entities.ServicesEntity;
 import com.example.a1.projecttest.R;
+import com.example.a1.projecttest.adapters.NewPagerFragmentAdapter;
+import com.example.a1.projecttest.fragments.ShcolnilFragment;
+import com.example.a1.projecttest.fragments.VospitannikFragment;
+import com.example.a1.projecttest.utils.ContentFactory;
+import com.example.a1.projecttest.vospitatel.fragments.RaspisanieFragment;
+
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EActivity(R.layout.zav_det_sada)
 public class MainZavDetSad extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     NavigationView navigationView;
     DrawerLayout drawer;
+
+    public FragmentManager getFragmentMan() {
+        FragmentManager fragmentManager;
+        fragmentManager = getSupportFragmentManager();
+        return fragmentManager;
+    }
+
+    public MainZavDetSad () {
+
+    }
+
     @AfterViews
     void main () {
 
@@ -37,7 +60,7 @@ public class MainZavDetSad extends AppCompatActivity implements NavigationView.O
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-        replaceFragment(new ServicesFragment(), R.id.content_main);
+        replaceFragment(new DirectoryFragment(), R.id.content_main);
     }
     public void replaceFragment(Fragment fragment, int id) {
         String backStackName = fragment.getClass().getName();
@@ -51,8 +74,12 @@ public class MainZavDetSad extends AppCompatActivity implements NavigationView.O
         }
     }
 
+
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
     }
+
+
 }
