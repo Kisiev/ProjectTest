849bb199d80bf07482de40a0661b92ec5f6cdcc4
Kisiev Valeri <kisivaleri@gmail.com>
2017-04-10 16:06:30 +0400

    addFunctionForDialog

--- app/src/main/java/com/example/a1/projecttest/fragments/FeedFragment.java
+++ app/src/main/java/com/example/a1/projecttest/fragments/FeedFragment.java
@@ -1,5 +1,6 @@
 package com.example.a1.projecttest.fragments;
 
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -7,9 +8,13 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.a1.projecttest.MainActivity;
@@ -23,7 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class FeedFragment extends Fragment{
+public class FeedFragment extends Fragment implements View.OnClickListener{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -47,6 +52,24 @@ public class FeedFragment extends Fragment{
 
         return view;
     }
+    public void showDialog() {
 
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.reson_child_not_arrive);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogTheme;
+        final EditText nameButton = (EditText) dialog.findViewById(R.id.name_childET);
+        Button photoButton = (Button) dialog.findViewById(R.id.add_child_buttonBT);
+        final Button addButton = (Button) dialog.findViewById(R.id.add_photo_buttonBT);
+        final Button cancelButton = (Button) dialog.findViewById(R.id.cancel_buttonBT);
+        photoButton.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.add_photo_buttonBT:
+
+                break;
+        }
+    }
 }
